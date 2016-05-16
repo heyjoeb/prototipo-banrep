f847584b78ca997330490459e4e8a2cd6c0d6256
heyjoeb <heyjoeb@hotmail.com>
2016-05-16 18:59:45 -0500

    Added scripts for offlines support and link navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,6 +16,7 @@
 	/* add style tile layer */
 	new L.tileLayer('https://api.mapbox.com/styles/v1/heyjoeb/cio4kq5k6004xafm06nm1pg4g/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGV5am9lYiIsImEiOiJjaW5vemYzeGgxMDUwdHZseXVicXZwbTAzIn0.7GJ_d9Xk-m50NUgRsOcnXg', {
 		maxZoom: 20,
+		useCache: true,
 		tileSize: 512,
   		zoomOffset: -1,
 		attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -35,9 +36,13 @@
 
 	// ADD MARKERS
 	var marker = L.marker([13.366427, -81.384776], {icon: orangeIcon}).addTo(map);
+	var marker2 = L.marker([13.369427, -81.385776], {icon: orangeIcon}).addTo(map);
+	var marker3 = L.marker([13.352427, -81.365776], {icon: orangeIcon}).addTo(map);
 
 	// ADD POPUP
 	marker.bindPopup('Acá queda algo que se llama EL PULPO').openPopup();
+	marker2.bindPopup('Acá queda algo que se llama EL PULPO2').openPopup();
+	marker3.bindPopup('Acá queda algo que se llama EL PULPO3').openPopup();
 
 	// ADD SIDEBAR
 	var sidebar = $('#sidebar').sidebar();
@@ -48,4 +53,15 @@
 	var osm2 = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 13, attribution: osmAttrib });
 	var miniMap = new L.Control.MiniMap(osm2, { toggleDisplay: true }).addTo(map);
 
+	//  ADD LINK FOR MAP NAVIGATION
+	document.getElementById('map-navigation').onclick = function(abc) {
+        var pos = abc.target.getAttribute('data-position');
+        var zoom = abc.target.getAttribute('data-zoom');
+        if (pos && zoom) {
+            var locat = pos.split(',');
+            var zoo = parseInt(zoom);
+            map.setView(locat, zoo, {animation: true});
+            return false;
+        }
+    }
 }(window, document, L));
